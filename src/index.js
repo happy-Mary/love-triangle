684991ca2e9eae92e0de32933ba4ee4443f04111
Maria <mariashklovskaya@coherentsolutions.com>
2018-02-20 07:52:24 +0300

    rolling-scope/feature: love triangle

--- src/index.js
+++ src/index.js
@@ -4,4 +4,19 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  preferences.unshift('move');
+  let count = 0;
+  for(let firstIndex = 1; firstIndex < preferences.length;) {
+    let secondIndex = preferences[firstIndex];
+    let thirdIndex = preferences[secondIndex];
+    if (preferences[thirdIndex] === firstIndex && secondIndex !== thirdIndex) {
+      count += 1;
+      firstIndex += 3;
+    } else {
+      firstIndex += 3;
+    }
+  }
+  return count;
 };
+
+let arr = [2, 3, 1, 9, 8, 6, 3, 8, 1, 5];
