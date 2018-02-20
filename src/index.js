814bf42a6805f43a3302aadc20dda15ea5aabd15
Maria <mariashklovskaya@coherentsolutions.com>
2018-02-20 13:08:41 +0300

    rolling-scopes/feature: triangle finished

--- src/index.js
+++ src/index.js
@@ -8,33 +8,17 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
   let count = 0;
   let indexArr = [];
   for(let firstIndex = 1; firstIndex < preferences.length;) {
-    if(indexArr.includes(firstIndex)) {
-      firstIndex+=1;
-    } else {
+    if(!indexArr.includes(firstIndex)) {
       let secondIndex = preferences[firstIndex];
       let thirdIndex = preferences[secondIndex];
       let result = preferences[thirdIndex];
-      if (result === firstIndex) {
-        indexArr.push(firstIndex);
-        indexArr.push(secondIndex);
-        indexArr.push(thirdIndex);
+      if (result === firstIndex && firstIndex !== secondIndex) {
+        indexArr.push(firstIndex, secondIndex, thirdIndex);
         count+=1;
-        firstIndex+=1;
-      } else {
-        firstIndex+=1;
       }
     }
+    firstIndex+=1;
   }
   return count;
 };
 
-// let arr = ['h', 2, 3, 1, 5, 6, 4, 8, 9, 7, 11, 12, 10, 13, 9, 13, 2, 15, 13, 12, 11];
-// let secondIndex = preferences[firstIndex];
-//     let thirdIndex = preferences[secondIndex];
-//     let result = preferences[thirdIndex];
-//     if (result === firstIndex) {
-//       firstIndex += 3;
-//       count+=1;
-//     } else {
-//       firstIndex += 3;
-//     }
